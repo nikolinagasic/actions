6e9918da2051d252457aa7376217263dd0fb3a74
nikolinagasic <48239322+nikolinagasic@users.noreply.github.com>
2023-01-16 03:02:57 +0100

    Update settings.py

--- githubapp/settings.py
+++ githubapp/settings.py
@@ -7,8 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -68,7 +67,8 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
